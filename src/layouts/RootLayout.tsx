60aa51d0aa9821e967e57339f753b6153da0ceaf
MehranAbdi1379 <mehranabdi13793@gmail.com>
2023-04-24 18:35:09 +0330

    Fixing sidebar (fixed problem)

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -8,8 +8,13 @@ const RootLayout = () => {
   return (
     <>
       <Grid templateColumns="repeat(6 , 1fr)">
-        <GridItem colSpan={{ base: 6, md: 2, xl: 1 }} height="100vh">
-          <Sidebar></Sidebar>
+        <GridItem colSpan={{ base: 6, md: 2, xl: 1 }}>
+          <Box
+            position={{ base: "static", md: "fixed" }}
+            width={{ base: "100%", md: "33.2%", xl: "16.6%" }}
+          >
+            <Sidebar></Sidebar>
+          </Box>
         </GridItem>
 
         <GridItem colSpan={{ base: 6, md: 4, xl: 5 }} bg="gray.50">
