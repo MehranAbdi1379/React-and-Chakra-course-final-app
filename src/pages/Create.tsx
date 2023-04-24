688b3fc1a5bc00e45efad9b99603c9a62607b008
MehranAbdi1379 <mehranabdi13793@gmail.com>
2023-04-24 18:21:09 +0330

    Making create page (completely)

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,7 +1,55 @@
+import { SunIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+  useToast,
+} from "@chakra-ui/react";
 import React from "react";
+import { Form } from "react-router-dom";
 
 const Create = () => {
-  return <div>Create</div>;
+  const toast = useToast();
+
+  const onSubmit = () => {
+    toast({
+      title: "Task created",
+      colorScheme: "orange",
+      description: "The new task is created.",
+      duration: 3000,
+      isClosable: true,
+      icon: <SunIcon></SunIcon>,
+      position: "top",
+      variant: "top-accent",
+    });
+  };
+  return (
+    <Box>
+      <Form onSubmit={onSubmit}>
+        <FormControl isRequired p={5}>
+          <FormLabel>Task name:</FormLabel>
+          <Input type="text" width={400}></Input>
+        </FormControl>
+        <FormControl p={5}>
+          <FormLabel>Task description:</FormLabel>
+          <Textarea
+            placeholder="Enter a detailed description for the task..."
+            width={400}
+          ></Textarea>
+        </FormControl>
+        <FormControl p={5}>
+          <Checkbox colorScheme="orange">Make this a priority task</Checkbox>
+        </FormControl>
+        <FormControl p={5}>
+          <Button type="submit">Submit</Button>
+        </FormControl>
+      </Form>
+    </Box>
+  );
 };
 
 export default Create;
