b1948e8c151b6c645d23501543b8d8b8b7cdc49c
MehranAbdi1379 <mehranabdi13793@gmail.com>
2023-04-24 17:10:10 +0330

    Finishing dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,22 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { EditIcon, ViewIcon } from "@chakra-ui/icons";
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Divider,
+  Flex,
+  Grid,
+  GridItem,
+  HStack,
+  Heading,
+  Icon,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
@@ -16,10 +34,45 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <Grid>
+    <SimpleGrid minChildWidth="300px" spacing="40px" p="40px">
       {tasks &&
-        tasks.map((task) => <GridItem minWidth="250px" bg="white"></GridItem>)}
-    </Grid>
+        tasks.map((task) => (
+          <Card
+            key={task.id}
+            borderTop="8px"
+            borderColor="orange.400"
+            bg="white"
+          >
+            <CardHeader>
+              <HStack>
+                <Avatar src={task.img}></Avatar>
+                <Box>
+                  <Heading size="sm">{task.title}</Heading>
+                  <Text>by {task.author}</Text>
+                </Box>
+              </HStack>
+            </CardHeader>
+            <CardBody>
+              <Text color="gray">{task.description}</Text>
+            </CardBody>
+            <Divider borderColor="gray.300"></Divider>
+            <CardFooter gap={7}>
+              <Button variant="ghost" gap="35px">
+                <HStack>
+                  <ViewIcon></ViewIcon>
+                  <Text>Watch</Text>
+                </HStack>
+              </Button>
+              <Button variant="ghost">
+                <HStack>
+                  <EditIcon></EditIcon>
+                  <Text>Comment</Text>
+                </HStack>
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
+    </SimpleGrid>
   );
 };
 
