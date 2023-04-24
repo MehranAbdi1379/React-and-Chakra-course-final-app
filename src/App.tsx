817959893fdea317535b1469dec433caafa73660
MehranAbdi1379 <mehranabdi13793@gmail.com>
2023-04-24 15:12:50 +0330

    Setting up project and adding routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,23 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
+import RootLayout from "./layouts/RootLayout";
+import Dashboard from "./pages/Dashboard";
+import Create from "./pages/Create";
+import Profile from "./pages/Profile";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout></RootLayout>}>
+      <Route path="dashboard" element={<Dashboard></Dashboard>}></Route>
+      <Route path="create" element={<Create></Create>}></Route>
+      <Route path="Profile" element={<Profile></Profile>}></Route>
+    </Route>
+  )
+);
 
 function App() {
-  return <RouterProvider></RouterProvider>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import { Flex, Heading, Text } from "@chakra-ui/react";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <Flex>
+      <Heading></Heading>
+    </Flex>
+  );
+};
+
+export default Navbar;
