2f62d6bc71caee904392c0073387b20f91675400
MehranAbdi1379 <mehranabdi13793@gmail.com>
2023-04-24 16:22:49 +0330

    Finishing Bars

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,32 @@
-import { Flex, Heading, Text } from "@chakra-ui/react";
+import {
+  Avatar,
+  AvatarBadge,
+  Box,
+  Button,
+  Flex,
+  HStack,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
 
 const Navbar = () => {
   return (
-    <Flex>
-      <Heading></Heading>
+    <Flex justify="space-between" p="40px">
+      <Heading fontFamily="sans-serif" as="h1">
+        Dojo Tasks
+      </Heading>
+      <HStack>
+        <Avatar src="/img/mario.png">
+          <AvatarBadge boxSize="20px" bg="orange.500" borderWidth="0.15em">
+            <Text fontSize="0.7em" paddingBottom="3px" paddingLeft="1.5px">
+              3
+            </Text>
+          </AvatarBadge>
+        </Avatar>
+        <Text>mario@netninja.dev</Text>
+        <Button colorScheme="orange">Logout</Button>
+      </HStack>
     </Flex>
   );
 };
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,40 @@
+import { AtSignIcon, CalendarIcon, EditIcon } from "@chakra-ui/icons";
+import { Box, HStack, Heading, List, ListItem, Text } from "@chakra-ui/react";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <Box p="20px" bg="orange.300" height={{ base: "500px", md: "100vh" }}>
+      <List spacing={3} paddingTop={2}>
+        <ListItem>
+          <Link to="/">
+            <HStack color="white">
+              <CalendarIcon></CalendarIcon>
+              <Heading size="s">DashBoard</Heading>
+            </HStack>
+          </Link>
+        </ListItem>
+
+        <ListItem>
+          <Link to="create">
+            <HStack color="white">
+              <EditIcon></EditIcon>
+              <Heading size="s">New Task</Heading>
+            </HStack>
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link to="profile">
+            <HStack color="white">
+              <AtSignIcon></AtSignIcon>
+              <Heading size="s">Profile</Heading>
+            </HStack>
+          </Link>
+        </ListItem>
+      </List>
+    </Box>
+  );
 };
 
 export default Sidebar;
