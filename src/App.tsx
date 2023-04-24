eefa7c94f7af3a738423987d7f4937a15b375e82
MehranAbdi1379 <mehranabdi13793@gmail.com>
2023-04-24 15:00:01 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+function App() {
+  return <RouterProvider></RouterProvider>;
+}
+
+export default App;
